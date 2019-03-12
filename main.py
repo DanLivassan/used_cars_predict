c25ab682bb76831ab61d7d3c231661286baf2336
Danilo Santana <daniloxc@msn.com>
2019-03-12 07:44:02 -0300

    Analise mensal por tipo de carro

--- main.py
+++ main.py
@@ -12,17 +12,28 @@ df.sold_at = pd.to_datetime(df.sold_at)
 df.registration_date = pd.to_datetime(df.registration_date)
 df = df.query('price < 60000')
 
-"""
-for i in range(1, 12):
-    print("Mes {}:".format(i))
-    df_by_month = df[df.sold_at.dt.month == i]
-    qtd_cars = len(df_by_month)
-    cars_mean_price = df_by_month.price.mean()
-    if qtd_cars != 0:
-        print("Foram vendidos {} carros numa média de {} - Razao: {}".format(qtd_cars, cars_mean_price, cars_mean_price/qtd_cars))
-    else:
-        print("Não foram vendidos carros neste mês")
-"""
+
+car_types = list(df.car_type.unique())
+for ct in car_types:
+    dff = df.query('car_type == "{}"'.format(ct))
+    ar_c_m = []
+    for i in range(1, 13):
+        print("Mes {}:".format(i))
+        df_by_month = dff[dff.sold_at.dt.month == i]
+        qtd_cars = len(df_by_month)
+        cars_mean_price = df_by_month.price.mean()
+        if qtd_cars != 0:
+            ar_c_m.append(cars_mean_price/qtd_cars)
+        else:
+            ar_c_m.append(0)
+        if qtd_cars != 0:
+            print("Foram vendidos {} carros numa média de {} - Razao: {}".format(qtd_cars, cars_mean_price, cars_mean_price/qtd_cars))
+        else:
+            print("Não foram vendidos carros neste mês")
+    plt.plot(ar_c_m, label=ct)
+    plt.legend()
+
+plt.show()
 
 
 def normalize_features(features:list):
@@ -58,11 +69,8 @@ feature_w = []
 
 for i in range(1, 9):
     filtered = df[df['feature_{}'.format(i)]]
-    plt.xlabel('Price')
-    plt.ylabel('Feature {}'.format(i))
-    #sns.distplot(filtered.price)
     feature_w.append({'mean':float(filtered.price.mean()), 'name': 'feature_{}'.format(i)})
-    #plt.show()
+
 
 df['feature_1'] = df['feature_1'].astype(float)
 df['feature_2'] = df['feature_2'].astype(float)
@@ -95,14 +103,13 @@ for f in feature_w:
 df['sum_features'] = (df['feature_1'] + df['feature_2']+ df['feature_3']+ df['feature_4']+ df['feature_5']+ df['feature_6']+ df['feature_7']+ df['feature_8'])*10
 y = df['price']
 X = df[[
-
     'mileage',
     'engine_power',
     'registration_date',
     'paint_color',
     'sum_features',
     'fuel',
-    'car_type',
+    #'car_type',
     'model_key'
 ]
 ]
@@ -110,7 +117,7 @@ X = df[[
 X['mileage'] = (X['mileage']-X['mileage'].min())/(X['mileage'].max()-X['mileage'].min())
 X['engine_power'] = (X['engine_power']-X['engine_power'].min())/(X['engine_power'].max()-X['engine_power'].min())
 X['registration_date'] = (X['registration_date']-X['registration_date'].min())/(X['registration_date'].max()-X['registration_date'].min())
-print(X.head())
+
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
@@ -126,8 +133,8 @@ from sklearn import metrics
 print('MAE:', metrics.mean_absolute_error(y_test, y_pred))
 print('MSE:', metrics.mean_squared_error(y_test, y_pred))
 print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-#plt.scatter(y_test, y_pred)
-#plt.xlabel('Y Test')
-#plt.ylabel('Predicted Y')
-sns.distplot(y_test-y_pred)
+plt.scatter(y_test, y_pred)
+plt.xlabel('Y Test')
+plt.ylabel('Predicted Y')
+#sns.distplot(y_test-y_pred)
 plt.show()
